24c4e8a1ec7fddfaa55928dea2cdadd662bc8fad
Simon Musall <49614434+musall@users.noreply.github.com>
2021-03-26 12:31:23 +0100

    Update vStim_TestImage.m

--- VisualStim/Paradigms/vStim_TestImage.m
+++ VisualStim/Paradigms/vStim_TestImage.m
@@ -23,6 +23,7 @@ CrossWidth = BasicVarVals(ismember(BasicVarNames,'CrossWidth'));
 
 % Open PTB screen window
 PsychDefaultSetup(1);
+Screen('Preference', 'SkipSyncTests', 1)
 screenNumber = max(Screen('Screens')); % Draw to the external screen if avaliable
 
 window = Screen('OpenWindow', screenNumber, 0); %open ptb window and save handle in pSettings
@@ -34,10 +35,27 @@ handles.ScreenRes.String = [num2str(screenXpixels) ' x ' num2str(screenYpixels)]
 Priority(MaxPriority(window));
 [screenXpixels,screenYpixels] = Screen('WindowSize', window); % Get the size of the current window
 
+% get cm coordinates
+screenSize = textscan(handles.ScreenSize.String,'%f%c%f'); %get screen size in cm
+screenWidth = screenSize{1};
+screenHeight = screenSize{3};
+
 %% Draw image
 Screen('FrameRect', window, [], [], BasicVarVals(ismember(BasicVarNames,'FrameWdith')));
 Screen('FillRect', window,255,[round(screenXpixels/2)-(CrossWidth/2) 0 round(screenXpixels/2)+(CrossWidth/2) screenYpixels]);
 Screen('FillRect', window,255,[0 round(screenYpixels/2)-(CrossWidth/2) screenXpixels round(screenYpixels/2)+(CrossWidth/2)]);
+
+%draw horizontal cm lines.
+pxPerCM = screenYpixels / screenHeight;
+for x = 1 : floor(screenHeight)-1
+    Screen('FillRect', window,128,[0 round(pxPerCM*x) screenXpixels round(pxPerCM*x)+5]);
+end
+
+pxPerCM = screenXpixels / screenWidth;
+for x = 1 : floor(screenWidth)-1
+    Screen('FillRect', window,128,[round(pxPerCM*x) 0 round(pxPerCM*x)+5 screenYpixels]);
+end
+
 Screen('Flip', window); %
 handles.Status.String = 'Testimage - Press any key to release'; 
 drawnow();
